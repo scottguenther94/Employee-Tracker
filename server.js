3815866ad896e29884f5e932913540c6ee27d058
scottguenther94 <scottguenther94@gmail.com>
2021-11-17 17:58:01 -0500

    Added functions to populate arrays with db data

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Require mySQL, console.table, Inquirer and establish PORT, db
 const mysql = require('mysql2');
 const conTable = require('console.table');
 const inquirer = require('inquirer');
@@ -11,3 +12,49 @@ const db = mysql.createConnection(
     }
 );
 require('dotenv').config();
+// Inquirer prompt arrays
+let employees = [];
+let roles = [];
+let departments = [];
+let managers = [];
+
+// Function to populate the employee array with db data
+function employeeArray() {
+    employees = [];
+    db.query('SELECT * FROM employee', (err, res) => {
+        if(err) throw err;
+        for (var i=0; i<res.length; i++) {
+            employees.push(res[i].first_name + " " + res[i].last_name);
+        }
+    })
+}
+// Function to populate the role array with db data
+function roleArray() {
+    roles = [];
+    db.query('SELECT * FROM role', (err, res) => {
+        if(err) throw err;
+        for (var i=0; i<res.length; i++) {
+            roles.push(res[i].title);
+        }
+    })
+}
+// Function to populate the department array with db data
+function departmentArray() {
+    departments = [];
+    db.query('SELECT * FROM department', (err, res) => {
+        if(err) throw err;
+        for (var i=0; i<res.length; i++) {
+            departments.push(res[i].name);
+        }
+    })
+}
+// Function to populate the role array with db data
+function managerArray() {
+    managers = [];
+    db.query('SELECT * FROM department', (err, res) => {
+        if(err) throw err;
+        for (var i=0; i<res.length; i++) {
+            managers.push(res[i].first_name + " " + res[i].last_name);
+        }
+    })
+}
