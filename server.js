29231f47ac096a5e1c9575c6275f017dca2068ca
scottguenther94 <scottguenther94@gmail.com>
2021-11-19 16:05:27 -0500

    created initial inquirer prompt

--- server.js
+++ server.js
@@ -65,3 +65,41 @@ function managerArray() {
         }
     })
 }
+
+function runApp() {
+    inquirer.prompt({
+        type: "list",
+        message: "Welcome! Please select an option below."
+        choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role", "exit"],
+        name: "firstOption"
+    })
+    .then(response => {
+        console.log(response.firstOption);
+
+        switch (response.firstOption) {
+            case "view all departments":
+            viewAllDepartments();
+            break;
+            case "view all roles":
+            viewAllRoles();
+            break;
+            case: "view all employees":
+            viewAllEmployees();
+            break;
+            case "add a department":
+            addDepartment();
+            break;
+            case "add a role":
+            addRole();
+            break;
+            case "add an employee":
+            addEmployee();
+            break;
+            case "update an employee role":
+            updateEmployee();
+            break;
+            default:
+            exit();
+        }
+    })
+}
