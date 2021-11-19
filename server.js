5adbb2f50ca6d5d50f99e918d22f0437bf14f7a7
Southern Comfort <scottguenther94@gmail.com>
2021-11-19 16:53:14 -0500

    database view options

--- server.js
+++ server.js
@@ -66,11 +66,11 @@ function managerArray() {
     })
 }
 
-function runApp() {
+function runInquirer() {
     inquirer.prompt({
         type: "list",
-        message: "Welcome! Please select an option below."
-        choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role", "exit"],
+        message: "Welcome! Please select an option below.",
+        choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role", "update an employee's manager", "exit"],
         name: "firstOption"
     })
     .then(response => {
@@ -83,7 +83,7 @@ function runApp() {
             case "view all roles":
             viewAllRoles();
             break;
-            case: "view all employees":
+            case "view all employees":
             viewAllEmployees();
             break;
             case "add a department":
@@ -98,8 +98,42 @@ function runApp() {
             case "update an employee role":
             updateEmployee();
             break;
+            case "update an employee's manager":
+            updateManager();
             default:
             exit();
         }
     })
+}
+// View data in table format based on user selection
+function viewAllDepartments() {
+    db.query('SELECT * FROM department', (err, departments) => {
+        if (err) throw err;
+        console.table(departments);
+        runInquirer();
+    })    
+}
+ 
+function viewAllRoles() {
+    db.query('SELECT role.id, role.title, department.name AS department, role.salary FROM role JOIN department ON role.department_id = department.id', (err, roles) => {
+        if (err) throw err;
+        console.table(roles);
+        runInquirer();
+    })
+}
+ 
+function viewAllEmployees() {
+    inquirer.prompt({
+        type: "list",
+        message: "How would you like to sort the list of employees?",
+        choices: ["by id", "by department", "by manager"],
+        name: "sortEmployees"
+    })
+        .then(response => {
+            db.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS department, CONCAT(m.first_name, " " ,  m.last_name) AS Manager FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id LEFT JOIN employee m on employee.manager_id = m.id ORDER ${response.sortEmployees}`, (err, employees) => {
+                if (err) throw err;
+                console.table(employees);
+                runInquirer();
+            })
+        })
 }
